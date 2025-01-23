4a79217ca377f049258dc471c633472b4cb78ee8
rams5623 <roboticramsfrcteam5623@gmail.com>
2025-01-23 16:46:11 -0500

    Created Flywheel subsystem

---
+++ src/main/java/frc/robot/subsystems/FlyWheel.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class FlyWheel extends SubsystemBase {
+  /* Create object containers */
+  private final SparkMax m_flyWheel;
+  private final SparkMaxConfig m_configMotor;
+
+  /** CREATE A NEW FLYWHEEL **/
+  public FlyWheel() {
+    /** Assign objects there classes and parameters **/
+    m_flyWheel = new SparkMax(20, MotorType.kBrushed);
+    m_configMotor = new SparkMaxConfig();
+    
+    /** Do the configuration setup for the motor **/
+    m_configMotor
+        .inverted(false)
+        .idleMode(IdleMode.kCoast);
+    
+    /** Apply the configuration to the motor **/
+    m_flyWheel.configure(m_configMotor, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
