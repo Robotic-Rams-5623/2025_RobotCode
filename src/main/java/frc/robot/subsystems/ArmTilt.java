1e0290a50590f62419624735e75765f828817150
Craig <45637739+ziehmerc@users.noreply.github.com>
2025-02-18 12:35:14 -0500

    ArmTilt.java Alternate Encoder Fixes

--- src/main/java/frc/robot/subsystems/ArmTilt.java
+++ src/main/java/frc/robot/subsystems/ArmTilt.java
@@ -5,7 +5,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMaxAlternateEncoder;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
@@ -19,62 +19,77 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ArmLengthConst;
-import frc.robot.Constants.ArmTiltConstants;
+
+import frc.robot.Constants.ArmConst.Tilt;
+import frc.robot.Constants.ArmConst.MotorConfigs;
 import frc.robot.Constants.CANSignals;
-import frc.robot.Constants.ArmLengthConst.kPositions;
+import frc.robot.Constants.ArmConst.kposition;
 
 public class ArmTilt extends SubsystemBase {
-  /** Creates a new ArmTilt. */
+  /* THIS SECTION CREATES ALL THE EMPTY OBJECTS FOR THIS SUBSYTEM */
+  // Create Motor Objects
   private final SparkMax m_armtilt;
-  private final SparkMaxConfig m_configMotor;
-  private final DigitalInput m_baseExtendLimit;
+  // Create Encoder Objects
+  // private final RelativeEncoder m_baseencoder;
+  private final SparkMaxAlternateEncoder m_baseencoder;
+  // Create Closed Loop Controller Objects
   private final SparkClosedLoopController m_basecontrol;
-  private final RelativeEncoder m_baseencoder;
-
+  // Create Motor Configuration Objects
+  private final SparkMaxConfig m_configMotor;
+  // Create Limit Switch Objects
+  private final DigitalInput m_baseExtendLimit;
+  // Create Trapezoidal closed loop profile Objects
   private TrapezoidProfile m_Profilebot;
   private TrapezoidProfile.State goalbot;
   private TrapezoidProfile.State setpointbot;
   private Timer m_Timer;
   private double m_setpointbot;
 
-
+  /* CREATE A NEW ArmTilt SUBSYSTEM */
   public ArmTilt() {
-    m_armtilt = new SparkMax(ArmTiltConstants.kIDArmTiltMotor, MotorType.kBrushless);
+    /* THIS SECTION ASSIGNS STUFF TO THE CREATED OBJECTS */
+    // Define the motors (NEO Brushless plugged into Spark MAX)
+    m_armtilt = new SparkMax(Tilt.kIDArmTiltMotor, MotorType.kBrushless);
 
-   
-
+    // Define the encoders (Rev Throughbore quadrature encoders plugged into the Alt Encoder Data Port of Spark Max)
+    m_baseencoder = m_armtilt.getAlternateEncoder();
+    
+    // Define the motors configuration
     m_configMotor = new SparkMaxConfig();
     m_configMotor
         .inverted(false)
         .idleMode(IdleMode.kBrake)
         .openLoopRampRate(0.1)
         .closedLoopRampRate(0.1);
-    m_configMotor.encoder.apply(ArmLengthConst.kMotorEncoderConfig);
-    m_configMotor.closedLoop.apply(ArmLengthConst.kMotorLoopConfig);
-    m_configMotor.softLimit.apply(ArmLengthConst.kMotorSoftLimitConfig);
+    m_configMotor.encoder.apply(MotorConfigs.kAltEncoderConfig_NEO);
+    m_configMotor.closedLoop.apply(MotorConfigs.kMotorLoopConfig_NEO);
+    m_configMotor.softLimit.apply(MotorConfigs.kMotorSoftLimitConfig_Base);
     m_configMotor.signals.apply(CANSignals.ArmMotors.kMotorSignalConfig);
 
+    // Apply the motor configurations to the motors
     m_armtilt.configure(m_configMotor, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
 
-    m_baseencoder = m_armtilt.getAlternateEncoder();
-    m_baseencoder.setPosition(0.0);
-    
-    m_basecontrol = m_armtilt.getClosedLoopController();
-    
-    m_baseExtendLimit = new DigitalInput(ArmLengthConst.kDIOBaseExtendSwitch);
+     // Reset Encoder to Zero
+    m_baseencoder.setPosition(2.0); // Set the current position as the starting position
 
+    // Get the closed loop controllers from the motors
+    m_basecontrol = m_armtilt.getClosedLoopController();
+
+    // Configure the LOWER LIMIT limit switch.
+    m_baseExtendLimit = new DigitalInput(Tilt.kDIOBaseExtendSwitch);
+
+    // Configure the items needed for trapezoidal profiling
     m_Timer = new Timer();
     m_Timer.start();
     m_Timer.reset();
     
-    m_setpointbot = kPositions.setpoint[0][1];
+    m_setpointbot = kposition.setpoint[0][0];
 
-    m_Profilebot = new TrapezoidProfile(ArmLengthConst.kArmMotionConstraint);
-    
+    m_Profilebot = new TrapezoidProfile(Tilt.kArmMotionConstraint);
     goalbot = new TrapezoidProfile.State();
     setpointbot = new TrapezoidProfile.State();
 
+    // Update the current motion profile with the current position.
     updateMotionprofile();
   }
 
@@ -106,11 +121,11 @@ public class ArmTilt extends SubsystemBase {
   }
 
   public void up() {
-    m_armtilt.set(ArmTiltConstants.kSpeedUp);
+    m_armtilt.set(Tilt.kSpeedUp);
   }
 
   public void down() {
-    m_armtilt.set(-ArmTiltConstants.kspeedDown);
+    m_armtilt.set(-Tilt.kspeedDown);
   }
 
   public void halt() {
@@ -135,8 +150,7 @@ public class ArmTilt extends SubsystemBase {
      * Set the position of both arms simultaneously to move in fluid motion to desired position.
      * A feedforward could be calculated and added to the top control if gravity starts to fight us.
      */
-     //m_basecontrol.setReference(kPositions.setpoint[posID][0], ControlType.kPosition);
-    m_basecontrol.setReference(kPositions.setpoint[posID][1], ControlType.kPosition);
+    m_basecontrol.setReference(kPositions.setpoint[posID][0], ControlType.kPosition);
   }
 
 
