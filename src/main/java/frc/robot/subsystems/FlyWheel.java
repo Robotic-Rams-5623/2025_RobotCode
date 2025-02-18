7614da5981b52c51e836ac7372b99695400fde7f
Craig <45637739+ziehmerc@users.noreply.github.com>
2025-02-18 12:40:50 -0500

    FlyWheel.java Formatting

--- src/main/java/frc/robot/subsystems/FlyWheel.java
+++ src/main/java/frc/robot/subsystems/FlyWheel.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
@@ -11,44 +7,51 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
-import frc.robot.Constants.FlyWheelConstants;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import frc.robot.Constants.FlyWheelConstants;
 
 public class FlyWheel extends SubsystemBase {
-  // /* Create object containers */
-  // private final SparkMax m_flyWheel;
-  // private final SparkMaxConfig m_configMotor;
+  /* THIS SECTION CREATES ALL THE EMPTY OBJECTS FOR THIS SUBSYTEM */
+  // Create Motor Objects
+  private final SparkMax m_flyWheel;
+  // Create Motor Configuration Objects
+  private final SparkMaxConfig m_configMotor;
+  // Create Limit Switch Objects
   private final DigitalInput m_coralSwitch;
 
   /** CREATE A NEW FLYWHEEL **/
   public FlyWheel() {
-    // /** Assign objects there classes and parameters **/
-    // m_flyWheel = new SparkMax(FlyWheelConstants.kIDFlyWheelMotor, MotorType.kBrushed);
-    // m_configMotor = new SparkMaxConfig();
-    
-    // /** Do the configuration setup for the motor **/
-    // m_configMotor
-    //     .inverted(false)
-    //     .idleMode(IdleMode.kCoast);
-    
-    // /** Apply the configuration to the motor **/
-    // m_flyWheel.configure(m_configMotor, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+    /* THIS SECTION ASSIGNS STUFF TO THE CREATED OBJECTS */
+    // Define the motors (Rev HD Hex Brushed plugged into Spark MAX)
+    m_flyWheel = new SparkMax(FlyWheelConstants.kIDFlyWheelMotor, MotorType.kBrushed);
 
-    /* CORAL SWITCH CONFIGURATION */
+    // Define the motors configuration
+    m_configMotor = new SparkMaxConfig();
+    m_configMotor
+        .inverted(false)
+        .idleMode(IdleMode.kCoast);
+    m_configMotor.signals.apply(CANSignals.HandMotors.kMotorSignalConfig_Dumb);
+    
+    // Apply the motor configurations to the motors
+    m_flyWheel.configure(m_configMotor, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+
+    // Configure the LOWER LIMIT limit switch.
     m_coralSwitch = new DigitalInput(FlyWheelConstants.kDIOSwitch);
   }
 
   public void in() {
-    // m_flyWheel.set(FlyWheelConstants.kSpeedIn);
+    m_flyWheel.set(FlyWheelConstants.kSpeedIn);
   }
 
   public void out() {
-    // m_flyWheel.set(-FlyWheelConstants.kSpeedOut);
+    m_flyWheel.set(-FlyWheelConstants.kSpeedOut);
   }
 
   public void stop() {
-    // m_flyWheel.set(0.0);
+    m_flyWheel.set(0.0);
   }
 
   public boolean getSwitch() {
@@ -58,5 +61,6 @@ public class FlyWheel extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putBoolean("Coral In?", getSwitch());
   }
 }
